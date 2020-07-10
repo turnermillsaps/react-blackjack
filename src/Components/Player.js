abe5867c15a4222ad2f2f8e7ac553bec643f4a7e
Chase Owens <cowens.owens@gmail.com>
2020-07-09 20:40:43 -0400

    Start with create-react-app

---
+++ src/Components/Player.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Player extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Player
