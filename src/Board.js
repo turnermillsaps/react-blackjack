c9054052ad73e8eff3892a0e9be9caa7738a52d6
Turner Millsaps <turnermillsaps.plp@gmail.com>
2020-07-21 19:48:40 -0400

    Fixed onclick for hit

--- src/Board.js
+++ src/Board.js
@@ -22,60 +22,62 @@ export class Board extends Component {
           dealerscore: 0
       }
     }
-      // Sum of cards
-      getCardValue = (card) => {
-        // Values of cards
-        const cardValues = {
-          KING: 10,
-          JACK: 10,
-          QUEEN: 10,
-          ACE: 11,
-          '10': 10,
-          '9': 9,
-          '8': 8,
-          '7': 7,
-          '6': 6,
-          '5': 5,
-          '4': 4,
-          '3': 3,
-          '2': 2
-        }
-        return cardValues[card];
+    // Sum of cards
+    getCardValue = (card) => {
+      // Values of cards
+      const cardValues = {
+        KING: 10,
+        JACK: 10,
+        QUEEN: 10,
+        ACE: 11,
+        '10': 10,
+        '9': 9,
+        '8': 8,
+        '7': 7,
+        '6': 6,
+        '5': 5,
+        '4': 4,
+        '3': 3,
+        '2': 2
       }
+      return cardValues[card];
+    }
 
-      // Drawing a new card
-      hit = () => {
-          if (this.state.playerscore < 21) {
-            fetch(`https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=1`)
-            .then(res => res.json())
-            .then(
-            (result) => {
-            console.log(result); 
-            const newCards = this.state.cards.push(result.cards[0])
-            this.setState({
-            playercards: newCards,
-            playerscore: (this.state.playerscore + this.getCardValue(result.cards[0].value))
-            })
-            this.blackjack();
-          }) 
-            
-          }
-        
-      }
-
-      // Check who has blackjack
-      blackjack = () => {
-        if (this.state.playerscore === 21) {
-          alert("You've Won!!");
-        } else if (this.state.playerscore > 21) {
-          alert("Bust!"); 
-        } 
-        if (this.state.dealerscore === 21) {
-          alert("Dealer Wins!");
-        } else if (this.state.dealerscore === 21 && this.state.playerscore === 21) {
-          alert("Draw!");
+    // Drawing a new card
+    hit = () => {
+        if (this.state.playerscore < 21) {
+          fetch(`https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=1`)
+          .then(res => res.json())
+          .then(
+          (result) => {
+          console.log(result); 
+          // const newCards = this.state.playercards.push(result.cards[0])
+          const newState = this.state;
+          newState.playercards.push(result.cards[0])
+          newState.playerscore = (this.state.playerscore + this.getCardValue(result.cards[0].value))
+          this.setState({
+            newState
+          })
+          this.blackjack();
+        }) 
+          
         }
+      
+    }
+
+    // Check who has blackjack
+    blackjack = () => {
+      if (this.state.playerscore === 21) {
+        alert("You've Won!!");
+      } else if (this.state.playerscore > 21) {
+        alert("Bust!"); 
+      } 
+      if (this.state.dealerscore === 21) {
+        alert("Dealer Wins!");
+      } else if (this.state.dealerscore === 21 && this.state.playerscore === 21) {
+        alert("Draw!");
       }
+    }
 
       // Dealing with Aces
       /*playerAce = () => {
@@ -89,58 +91,59 @@ export class Board extends Component {
 
     
             
-            componentDidMount = () => {
-              
-              // Getting a new deck
-              axios.get("https://deckofcardsapi.com/api/deck/new/")
+    componentDidMount = () => {
+      
+      // Getting a new deck
+      axios.get("https://deckofcardsapi.com/api/deck/new/")
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              deckID: result.data.deck_id
+            })
+            console.log(result);
+            // Shuffling the deck
+            fetch(
+              `https://deckofcardsapi.com/api/deck/${this.state.deckID}/shuffle/`
+            )
+            .then(res => res.json())
+            .then(json => {
+              console.log(json);
+              console.log('Shuffled!');
+            });       
+            // Drawing two cards for the player
+            fetch(
+              `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=2`
+            )
+            .then(res => res.json())
+            .then(
+              (result) => {
+                console.log(result); 
+
+                this.setState({
+                  playercards: result.cards,
+                  playerscore: this.getCardValue(result.cards[0].value) + this.getCardValue(result.cards[1].value)
+                }) 
+                // Drawing two cards for the dealer                      
+                fetch(
+                  `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=2`
+                )
+                .then(res => res.json())
                 .then(
                   (result) => {
-                    this.setState({
-                      isLoaded: true,
-                      deckID: result.data.deck_id
-                    })
                     console.log(result);
-                    // Shuffling the deck
-                    fetch(
-                      `https://deckofcardsapi.com/api/deck/${this.state.deckID}/shuffle/`
-                    )
-                    .then(res => res.json())
-                    .then(json => {
-                      console.log(json);
-                      console.log('Shuffled!');
-                    });       
-                    // Drawing two cards for the player
-                    fetch(
-                      `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=2`
-                    )
-                    .then(res => res.json())
-                    .then(
-                      (result) => {
-                        console.log(result); 
-
-                        this.setState({
-                          playercards: result.cards,
-                          playerscore: this.getCardValue(result.cards[0].value) + this.getCardValue(result.cards[1].value)
-                        }) 
-                        // Drawing two cards for the dealer                      
-                        fetch(
-                          `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/?count=2`
-                        )
-                        .then(res => res.json())
-                        .then(
-                          (result) => {
-                            console.log(result);
-                            this.setState({
-                              dealercards: result.cards,
-                              dealerscore: this.getCardValue(result.cards[0].value) + this.getCardValue(result.cards[1].value)
-                          })
-                          this.blackjack();
-                      }
-                    )}
-                  )
-                }
-              )
-            }
+                    this.setState({
+                      dealercards: result.cards,
+                      dealerscore: this.getCardValue(result.cards[0].value) + this.getCardValue(result.cards[1].value)
+                  })
+                  console.log(`current state: ${JSON.stringify(this.state)}`);
+                  // this.blackjack();
+              }
+            )}
+          )
+        }
+      )
+    }
 
     render = () => {
         return (
@@ -169,7 +172,7 @@ export class Board extends Component {
                     <h2 className='score-value'>*Username* Score {this.state.playerscore}</h2>
                   </div > 
                   <div className='player-box'>
-                    <button className='card-text'>Hit</button>
+                    <button className='card-text' onClick={this.hit}>Hit</button>
                     <button className='card-text'>Stand</button>   
                   </div>
                   <div>
